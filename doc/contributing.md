cf80da7d46c9c1d8da98dc915d3a3d18143a104b
longfangsong <longfangsong@icloud.com>
2018-12-09 23:51:05 +0800

    doc: Add contributing.md

---
+++ doc/contributing.md
@@ -0,0 +1,28 @@
+# Contributing to glpkxx
+
+## What should I know before I get started?
+
+### Tech
+
+#### Language
+
+C++, of course.
+
+#### Library
+
+[GLPK](http://www.gnu.org/software/glpk/).
+
+## How to get started
+
+Firstly, clone this library.
+
+Then make sure you've installed [GLPK](http://www.gnu.org/software/glpk/):
+
+```shell
+wget http://ftp.gnu.org/gnu/glpk/glpk-4.65.tar.gz
+tar -xf glpk-4.65.tar.gz
+cd glpk-4.65
+./configure
+make
+make install
+```
---
+++ test.cpp
@@ -0,0 +1,19 @@
+#include "library.h"
+
+int main() {
+    hello();
+//    LinerProblem problem;
+//    Variable x1("x1"), x2("x2"), x3("x3");
+//    x1 >= 0;
+//    x2 >= 0;
+//    x3 >= 0;
+//    problem.addConstraint(x1 + x2 + x3 <= 100);
+//    problem.addConstraint(10 * x1 + 4 * x2 + 5 * x3 <= 600);
+//    problem.addConstraint(2 * x1 + 2 * x2 + 6 * x3 <= 100);
+//    pair<double, map<Variable,double>> result = problem.minimize(10 * x1 + 6 * x2 + 4 * x3);
+//    cout << result.first << endl; // 733.333
+//    cout << result.second[x1] << endl; // 33.333
+//    cout << result.second[x2] << endl; // 66.667
+//    cout << result.second[x3] << endl; // 0
+    return 0;
+}
